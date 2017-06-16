c1a3eae303d9ad47b9cef2ab5bffc0d1f51dc7e1
Okwonks <albertoketch@gmail.com>
2017-06-16 12:45:26 +0300

    working on the js file business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
+//Business Logic
 var die = math.floor(math.random()*6 + 1);
+
+var players = [];
+
+function player(name) {
+  this.name = name;
+  this.total = [];
+}
+
+
+//User Interface Logic
+$(document).ready(function() {
+  $("#player-submit").submit(function(event) {
+    event.preventDefault();
+
+    var playerOne = $("input#player1").val();
+    var playerTwo = $("input#player2").val();
+  });
+
+  //here's where the game will be able to compute the values
+  $()
+})
