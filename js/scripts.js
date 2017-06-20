25edacfec22a869c44cdadb397796d50de5c7092
Okwonks <albertoketch@gmail.com>
2017-06-20 23:39:51 +0300

    finishing up on the user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -11,63 +11,116 @@ var holdNum = [];
 
 //User Interface Logic
 $(document).ready(function() {
-  $("div#player1-roll").hide();
-  $("div#player2-roll").hide();
-  $("form#players").submit(function(event) {
-    event.preventDefault();
+      $("div#player1-roll").hide();
+      $("div#player2-roll").hide();
+      $("form#players").submit(function(event) {
+        event.preventDefault();
 
-    var playerOne = $("input#player1").val();
-    var playerTwo = $("input#player2").val();
+        var playerOne = $("input#player1").val();
+        var playerTwo = $("input#player2").val();
 
 
-    //console.log(playerOne);
+        //console.log(playerOne);
 
-    var newPlayerOne = new Player(playerOne);
-    var newPlayerTwo = new Player(playerTwo);
-    console.log(newPlayerOne);
-    console.log(newPlayerTwo);
+        var newPlayerOne = new Player(playerOne);
+        var newPlayerTwo = new Player(playerTwo);
+        console.log(newPlayerOne);
+        console.log(newPlayerTwo);
 
-    players.push(newPlayerOne, newPlayerTwo);
-    $("h3#player1").text(players[0].playerName);
-    $("h3#player2").text(players[1].playerName);
-    $("div#player1-roll").show();
-    alert("Let's begin " + players[0].playerName);
-  });
-
-  //here's where the game will be able to compute the values
-  $("button#die1").click(function() {
-    var die = Math.floor(Math.random() * (6)) + 1;
-    //  alert(die);
-    if (die === 1) {
-      $("div#player2-roll").show();
-
-      holdNum = [];
-      $("h4#hold-total1").text("0");
-      alert(players[1].playerName + " your turn!")
-    } else {
-      holdNum.push(die);
-      var total = players[0].total.reduce(function(total, countNumber) {
-        return total + countNumber;
+        players.push(newPlayerOne, newPlayerTwo);
+        $("h3#player1").text(players[0].playerName);
+        $("h3#player2").text(players[1].playerName);
+        $("div#player1-roll").show();
+        alert("Let's begin " + players[0].playerName);
       });
-      $("h4#hold-total1").text(rollScore);
-    }
-  });
 
-  $("button#hold1").click(function() {
-    var total = holdNum.reduce(function(total, countNumber) {
-      return total + countNumber;
-    })
-    players[0].rollScore.push(total);
-    var score = players[0].reduce(function(total, countNumber) {
-      return total + countNumber;
-    });
-    players[0].total = score;
-    $("h2#score1").text(score);
+      //here's where the game will be able to compute the values
 
-    $("div#player1-roll").fadeOut(3000);
-    $("div#player2-roll").fadeIn(3000);
+      /* let's start with player one's code
+       the roll and hold buttons */
+      $("button#die1").click(function() {
+        var die = Math.floor(Math.random() * (6)) + 1;
+        $("h2#die-count").text(die);
 
-    alert(players[1].playerName + " your turn!");
+        if (die === 1) {
+          $("div#player2-roll").show();
 
-  })
-}) //end of document User Interface Logic
+          holdNum = [];
+          $("h4#hold-total1").text("0");
+          alert(players[1].playerName + " your turn!")
+        } else {
+          holdNum.push(die);
+          var total = players[0].total.reduce(function(total, countNumber) {
+            return total + countNumber;
+          });
+          $("h4#hold-total1").text(rollScore);
+        }
+      });
+
+      $("button#hold1").click(function() {
+        var total = holdNum.reduce(function(total, countNumber) {
+          return total + countNumber;
+        })
+        players[0].rollScore.push(total);
+        var score = players[0].reduce(function(total, countNumber) {
+          return total + countNumber;
+        });
+        players[0].total = score;
+        $("h2#score1").text(score);
+
+        $("div#player1-roll").fadeOut(3000);
+        $("div#player2-roll").fadeIn(3000);
+
+        alert(players[1].playerName + " your turn!");
+
+        if (score >= 100) {
+          alert(players[0].playerName + " wins!!!");
+          $("div#player1-roll").fadeOut(3000);
+          $("div#player2-roll").fadeOut(3000);
+        }
+
+      });
+
+      /* then we can deal with player two's code from here
+      it's goig to deal with the hold and roll */
+      $("button#die2").click(function() {
+        var die = Math.floor(Math.random() * (6)) + 1;
+        $("h2#die").text(die);
+        if (die === 1) {
+          $("div#player1-roll").fadeIn();
+
+          holdNum = [];
+          $("h4#hold-total2").text("0");
+          alert(players[1].playerName + " your turn!")
+        } else {
+          holdNum.push(die);
+          var total = players[0].total.reduce(function(total, countNumber) {
+            return total + countNumber;
+          });
+          $("h4#hold-total2").text(rollScore);
+        }
+      });
+
+      $("button#hold2").click(function() {
+        var total = holdNum.reduce(function(total, countNumber) {
+          return total + countNumber;
+        })
+        players[1].rollScore.push(total);
+        var score = players[1].reduce(function(total, countNumber) {
+          return total + countNumber;
+        });
+        players[1].total = score;
+        $("h2#score2").text(score);
+
+        $("div#player2-roll").fadeOut(3000);
+        $("div#player1-roll").fadeIn(3000);
+
+        alert(players[0].playerName + " your turn!");
+
+        if (score >= 100) {
+          alert(players[1].playerName + " wins!!!");
+          $("div#player1-roll").fadeOut(3000);
+          $("div#player2-roll").fadeOut(3000);
+        }
+
+      }) //end of document User Interface Logic
