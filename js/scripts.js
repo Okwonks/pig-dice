1ea9ef3b4ba1a9cc51fc521e55c8e896e8a69b1c
Okwonks <albertoketch@gmail.com>
2017-06-20 08:44:38 +0300

    adding changes to the css, index.html and scripts.js files.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@
 var players = [];
 
 function Player(name) {
-  this.name = name;
+  this.playerName = name;
   this.total = [];
 }
 
 
 //User Interface Logic
 $(document).ready(function() {
+  $("div#player1-roll").hide();
+  $("div#player2-roll").hide();
   $("form#players").submit(function(event) {
     event.preventDefault();
 
     var playerOne = $("input#player1").val();
     var playerTwo = $("input#player2").val();
+
 
     //console.log(playerOne);
 
@@ -23,12 +26,16 @@ $(document).ready(function() {
     console.log(newPlayerTwo);
 
     players.push(newPlayerOne, newPlayerTwo);
+    $("h3#player1").text(players[0].playerName);
+    $("h3#player2").text(players[1].playerName);
+    $("div#player1-roll").show()
   });
 
   //here's where the game will be able to compute the values
     $("button#player-roll").click(function() {
       var die = Math.floor(Math.random()*(6)) + 1;
     //  alert(die);
+
       players[0].total.push(die);
       var score = players[0].total.reduce(function(total, countNumber) {
         return total + countNumber;
@@ -41,5 +48,5 @@ $(document).ready(function() {
       console.log(score);
       alert(score);
 
-  })  
+  })
 }) //end of document User Interface Logic
